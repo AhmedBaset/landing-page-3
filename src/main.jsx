0c872368713f9b596e7c40b02b45f92117ab8e9b
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2022-09-16 21:44:08 +0200

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { HashRouter } from "react-router-dom";
+console.log(HashRouter);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<HashRouter base="/">
+			<App />
+		</HashRouter>
+	</React.StrictMode>
+);
