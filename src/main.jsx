a5e51aa3aa1790254f9b9be97815c6236fe2457b
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2022-09-16 21:45:16 +0200

    Hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { HashRouter } from "react-router-dom";
-console.log(HashRouter);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<HashRouter base="/">
-			<App />
-		</HashRouter>
-	</React.StrictMode>
+	<HashRouter base="/">
+		<App />
+	</HashRouter>
 );
