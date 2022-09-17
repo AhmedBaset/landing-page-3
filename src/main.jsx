f40197f2f292d9ff4ed7029d4ce8e0329f288f1f
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2022-09-17 10:44:37 +0200

    alhamdulellah

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+
 console.log("done");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<HashRouter base="/template-react-stylish">
+	<React.StrictMode>
 		<App />
-	</HashRouter>
+	</React.StrictMode>
 );
