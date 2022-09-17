c73bc8a0d4dcb4c0c448e9c25fb14f2ec822861d
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2022-09-17 10:20:40 +0200

    root

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<HashRouter base="/">
+	<HashRouter base="/template-react-stylish">
 		<App />
 	</HashRouter>
 );
