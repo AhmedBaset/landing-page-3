7211d2bd501a4a3ae0ac56331a6b7b6893be3c93
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2022-09-17 10:21:56 +0200

    log

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+console.log("done");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<HashRouter base="/template-react-stylish">
